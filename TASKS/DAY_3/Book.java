432f4dbdb9b295f67155916046df942bdac2c7c5
VIKASHINIDEVARAj <vikashinidevaraj27@gmail.com>
2025-03-10 21:25:57 +0530

    Update Book.java

--- TASKS/DAY_3/Book.java
+++ TASKS/DAY_3/Book.java
@@ -1,29 +1,29 @@
 public class Book {
-    String title = "";
-    String author = "";
-   
-    Book(){
-        title = "Pride and Prejudice";
-        author = "Jane Austen";
+    String title;
+    String author;
+    public Book() {
+        this.title = "LIFE";
+        this.author = "VIKXX";
     }
-   
-    Book(String mytitle, String myauthor){
-        this.title = mytitle;
-        this.author = myauthor;
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
     }
-   
     public void displayDetails() {
         System.out.println("Title: " + title);
         System.out.println("Author: " + author);
     }
-   
+
     public static void main(String args[]) {
-       
-        Book obj1 = new Book();
-        obj1.displayDetails();
-       
-       
-        Book obj2 = new Book("Little Women", "Louisa May Alcott");
-        obj2.displayDetails();
+        Book book1 = new Book(); 
+        Book book2 = new Book("Little Women", "Louisa May Alcott");
+        Book book3 = new Book("1984", "George Orwell");
+        Book book4 = new Book("To Kill a Mockingbird", "Harper Lee");
+
+    
+        book1.displayDetails();
+        book2.displayDetails();
+        book3.displayDetails();
+        book4.displayDetails();
     }
 }
