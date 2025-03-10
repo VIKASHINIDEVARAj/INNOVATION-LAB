a3bd9db0c1bd2e22666ae0b95151e5e1ad18c1df
VIKASHINIDEVARAj <vikashinidevaraj27@gmail.com>
2025-03-10 21:51:07 +0530

    Add files via upload

---
+++ TASKS/DAY_3/Company.java
@@ -0,0 +1,29 @@
+public class Company {
+    static String companyName = "ZOHO";
+    String employeeName;
+    public Company(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+        public void displayDetails() {
+        System.out.println("Company: " + companyName);
+        System.out.println("Employee Name: " + employeeName);
+    }
+
+    public static void main(String[] args) {
+        
+        Company emp1 = new Company("VIKXX");
+        Company emp2 = new Company("ADI");
+        Company emp3 = new Company("YUVA");
+        System.out.println("Before modifying companyName:");
+        emp1.displayDetails();
+        emp2.displayDetails();
+        emp3.displayDetails();
+
+        Company.companyName = "WIPRO";
+        System.out.println("After modifying companyName:");
+        emp1.displayDetails();
+        emp2.displayDetails();
+        emp3.displayDetails();
+    }
+}
