27bd8958a0be259f3e34b7b4fb831e643dc32931
VIKASHINIDEVARAj <vikashinidevaraj27@gmail.com>
2025-03-19 17:25:52 +0530

    Update Vehicle.java

--- TASKS/DAY_4/Vehicle.java
+++ TASKS/DAY_4/Vehicle.java
@@ -1,4 +1,26 @@
-<<<<<<< HEAD
+class Vehicle {
+    void start() {
+        System.out.println("VEHICLE");
+    }
+}
+class Car extends Vehicle {
+    void start() {
+        System.out.println("The car");
+    }
+}
+class Bike extends Vehicle {
+    void start() {
+        System.out.println("The bike");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle vehicle1 = new Car();
+        Vehicle vehicle2 = new Bike();
+        vehicle1.start();
+        vehicle2.start();
+    }
 class Vehicle {
     void start() {
         System.out.println("VEHICLE");
@@ -23,29 +45,3 @@ public class Main {
         vehicle2.start();
     }
 }
-=======
-class Vehicle {
-    void start() {
-        System.out.println("VEHICLE");
-    }
-}
-class Car extends Vehicle {
-    void start() {
-        System.out.println("The car");
-    }
-}
-class Bike extends Vehicle {
-    void start() {
-        System.out.println("The bike");
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        Vehicle vehicle1 = new Car();
-        Vehicle vehicle2 = new Bike();
-        vehicle1.start();
-        vehicle2.start();
-    }
-}
->>>>>>> d4b784a1a2afeb66c2a26fd79cea05049f33afe4
